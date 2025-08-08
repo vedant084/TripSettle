a496323a9ab0a58cdabe261e97bac128e4c6aa12
Vedant Salvi <vedantsalvi57@gmail.com>
2025-08-08 16:05:19 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from werkzeug.middleware.proxy_fix import ProxyFix
 
+
 # Set up logging
 logging.basicConfig(level=logging.DEBUG)
 
